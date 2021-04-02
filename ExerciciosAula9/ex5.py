512c045d884d33ce2c7c7ce5a6180b17ac275ff5
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-02 19:27:52 -0300

    Exercicio 5 finalizado!

---
+++ ExerciciosAula9/ex5.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite um numero inteiro: "))
+n2 = int(input("Digite o segundo numero inteiro: "))
+n3 = int(input("Digite mais um numero inteiro: "))
+
+if(n1>n2>n3):
+    print("Os numeros foram digitados em ordem crescente!")
+else:
+    print("Os numeros nao foram digitados em ordem crescente!")
+
+    
