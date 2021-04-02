9cc0e384423f38af335164d1b4782e6eccc2b8ff
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-02 18:09:37 -0300

    correção do teste de mesa!

--- ExerciciosAula8/testeDeMesa.py
+++ ExerciciosAula8/testeDeMesa.py
@@ -7,5 +7,5 @@ print(resultado)
 
 
 #Qual o conteúdoda variável resultado quando termina o algoritmo? 20
-#Analise o pseudocódigoe responda: o que mostrará na tela? 20
+#Analise o pseudocódigoe responda: o que mostrará na tela? Resultado maior que zero e menor ou igual a 20
 #Reescrever o algoritmo utilizando a linguagem de programaçãoPython.
