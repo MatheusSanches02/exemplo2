94718e7d16dbf83cf6c5ee6ee2d6bcfdeb83505c
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-02 18:08:06 -0300

    Teste de mesa finalizado!

---
+++ ExerciciosAula8/testeDeMesa.py
@@ -0,0 +1,11 @@
+numero_1 = 15
+numero_2 = 6
+
+resultado= ((numero_1%2)*3)+(13-2+numero_2)
+
+print(resultado)
+
+
+#Qual o conteúdoda variável resultado quando termina o algoritmo? 20
+#Analise o pseudocódigoe responda: o que mostrará na tela? 20
+#Reescrever o algoritmo utilizando a linguagem de programaçãoPython.
