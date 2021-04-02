4922375d83a7b16b2354067598666095279ce169
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-02 18:56:38 -0300

    segundo exercicio finalizado

---
+++ ExerciciosAula9/ex2.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Digite um numero inteiro: "))
+numero2 = int(input("Digite outro numero inteiro: "))
+
+if(numero1 > numero2):
+    print(numero1, " é maior que ", numero2)
+else:
+    print(numero2," é maior que ", numero1)
+
+    
