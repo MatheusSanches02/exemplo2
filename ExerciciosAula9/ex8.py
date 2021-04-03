327d37cd42f15b82080f28b4c61dba5c070a2ecb
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-02 22:43:49 -0300

    exercicio 8 finalizado e finalização total! do exercicio

---
+++ ExerciciosAula9/ex8.py
@@ -0,0 +1,10 @@
+numero1 = float(input("Digite um numero real: "))
+numero2 = float(input("Digite outro numero real: "))
+
+soma = numero1 +numero2
+
+if(soma >10.0):
+    print("Numero maior que 10")
+else:
+    print("Numero menor que 10")
+
