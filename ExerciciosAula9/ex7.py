b5271755f7e9fbccef7a16d3b0bf9144568d60a5
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-02 22:03:45 -0300

    exercicio 7 finalizado!

---
+++ ExerciciosAula9/ex7.py
@@ -0,0 +1,32 @@
+numero = int(input("Digite um numero inteiro: "))
+multiplo = numero % 5
+multiplo1 = numero % 10
+
+if(multiplo == 0) and (multiplo1 == 0):
+    print("é multiplo de 5 e 10 ao mesmo tempo")
+else:
+    print("não é multiplo de 5 ou 10")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
