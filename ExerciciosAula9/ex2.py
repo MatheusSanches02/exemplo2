d32d0aa7b6c7eb4589aa4f857dc279d960411738
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-12 09:07:26 -0300

    Algumas correções de codigo

--- ExerciciosAula9/ex2.py
+++ ExerciciosAula9/ex2.py
@@ -3,7 +3,8 @@ numero2 = int(input("Digite outro numero inteiro: "))
 
 if(numero1 > numero2):
     print(numero1, " é maior que ", numero2)
-else:
+elif(numero1 < numero2):
     print(numero2," é maior que ", numero1)
-
+else:
+    print("Os números digitados são iguais!")
     
