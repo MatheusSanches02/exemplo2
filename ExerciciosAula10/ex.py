74682d593c92720d4f795aa63a6f40a0f0426c24
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-14 14:12:51 -0300

    Exercicio proposto aula 10

---
+++ ExerciciosAula10/ex.py
@@ -0,0 +1,21 @@
+cont_par= 2
+cont_impar= 1
+soma_par= 0
+soma_impar= 0
+print("---Caro(a) Professor(a), entre com as notas dos alunos ---")
+while cont_impar<= 50:
+    print("Você está digitando as notas dos alunos ímpares. Por favor, insira a nota do aluno de número ", cont_impar)
+    nota = float(input("--> "))
+    while nota < 0 or nota > 10:
+        print("Você está digitando as notas dos alunos ímpares. Por favor, insira a nota do aluno de número", cont_impar)
+    nota = float(input("Nota inválida! Digite novamente!"))
+    soma_impar+= nota
+    cont_impar+= 2
+while cont_par<= 50:
+    print("Você está digitando as notas dos alunos pares. Por favor, insira a nota do aluno de número ", cont_par)
+nota = float(input("--> "))
+while nota < 0 or nota > 10:
+    print("Você está digitando as notas dos alunos pares. Por favor, insira a nota do aluno de número ",cont_par)
+    nota = float(input("Nota inválida! Digite novamente!"))
+    soma_par+=nota
+    cont_par+=2
