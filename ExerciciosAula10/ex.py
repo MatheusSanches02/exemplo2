0431d136d55ea14a948445f9e01c7ff1b879cb18
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-18 15:17:45 -0300

    Exercicio de preparaçao dos laços de repetição

--- ExerciciosAula10/ex.py
+++ ExerciciosAula10/ex.py
@@ -8,15 +8,15 @@ while cont_impar<= 50:
     nota = float(input("--> "))
     while nota < 0 or nota > 10:
         print("Você está digitando as notas dos alunos ímpares. Por favor, insira a nota do aluno de número", cont_impar)
-    nota = float(input("Nota inválida! Digite novamente!"))
+        nota = float(input("Nota inválida! Digite novamente!"))
     soma_impar+= nota
     cont_impar+= 2
 while cont_par<= 50:
     print("Você está digitando as notas dos alunos pares. Por favor, insira a nota do aluno de número ", cont_par)
-nota = float(input("--> "))
-while nota < 0 or nota > 10:
-    print("Você está digitando as notas dos alunos pares. Por favor, insira a nota do aluno de número ",cont_par)
-    nota = float(input("Nota inválida! Digite novamente!"))
+    nota = float(input("--> "))
+    while nota < 0 or nota > 10:
+        print("Você está digitando as notas dos alunos pares. Por favor, insira a nota do aluno de número ",cont_par)
+        nota = float(input("Nota inválida! Digite novamente!"))
     soma_par+=nota
     cont_par+=2
 media_par= soma_par/ 25
