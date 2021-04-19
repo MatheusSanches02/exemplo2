da0f3ae1b7c589bc38a13335bf252f45dd52d9ea
MatheusSanches02 <rm87426@fiap.com.br>
2021-04-19 15:18:55 -0300

    Correção de exercicios

--- ExerciciosAula10/ex4.py
+++ ExerciciosAula10/ex4.py
@@ -3,19 +3,16 @@ num2 = int(input("Digite outo numero: "))
 
 c = 0
 
+
+if num1 > num2:
+    aux = num1
+    num1 = num2
+    num2 = aux
+
+if num1 % 2 != 0:
+    num1+=1
+
 for n in range(num1, num2 + 1):
     if n % 2 != 0:
         c +=1
 print("A quantidade de impares entre ", num1 ,"e", num2, "eh: ", c)
-
-#- Inicio uma contador, representando a quantidade de ímpares, inicialmente com o valor 0
-#- Para cada número de n1 a n2, testo se ele não é par
-#- Se ele não for par, logo, é impar, então o contador é incrementado em 1
-#- No final, retorno o contador
-
-
-
-
-
-
-
